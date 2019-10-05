0a8ca3c20437988b220b6e1f64183f5260419ec5
eduardosukeda <eduardosukeda@live.com>
2019-10-05 14:31:13 -0300

    Calculadora

---
+++ Calculadora/src/TestaCalculadora.java
@@ -0,0 +1,33 @@
+import javax.swing.JOptionPane;
+
+public class TestaCalculadora {
+	public static void main(String[] args) {
+
+		String[] operacoes = { "Soma", "Subtração", "Multiplicação", "Divisão" };
+		int x = JOptionPane.showOptionDialog(null, "Returns the position of your choice on the array", "Click a button",
+				JOptionPane.DEFAULT_OPTION, JOptionPane.INFORMATION_MESSAGE, null, operacoes, operacoes[0]);
+		String sOp1;
+		float op1;
+		String sOp2;
+		float op2;
+		Calculadora calc = new Calculadora();
+
+		System.out.println(x);
+
+		sOp1 = JOptionPane.showInputDialog("Digite o primeiro numero");
+		op1 = Integer.parseInt(sOp1);
+		sOp2 = JOptionPane.showInputDialog("Digite o segundo numero");
+		op2 = Integer.parseInt(sOp2);
+
+		if (x == 0) {
+			System.out.println(calc.somar(op1, op2));
+		} else if (x == 1) {
+			System.out.println(calc.subtrair(op1, op2));
+		} else if (x == 2) {
+			System.out.println(calc.multiplicar(op1, op2));
+		} else if (x == 3) {
+			System.out.println(calc.dividir(op1, op2));
+		}
+
+	}
+}
